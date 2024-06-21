9a76c05919c267db93b23d1645c4f4b8d8cc54e7
Simoon66 <163728303+Simoon66@users.noreply.github.com>
2024-06-22 00:11:35 +0600

    Update config.py

--- config.py
+++ config.py
@@ -7,27 +7,26 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "26253678")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "7edc88177e309030bca59ae4cebc3011")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7301614681:AAHmRS1Pb_6lqqoJKuuiOxz5m7judwgkj3Q") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
 
-             # Don't Remove Credit @VJ_Botz
-             # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-             # Ask Doubt on telegram @KingVJ01
+             # Don't Remove Credit @abdulla_simoon
+             # Ask Doubt on telegram @abdulla_simoon
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://kamaluddin124578:6vFvXWrPM6jqyUbx@cluster0.ua2kw12.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7019013170').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
